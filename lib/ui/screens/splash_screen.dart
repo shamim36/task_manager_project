df1d75c0d8fde02b34f0523c95766bf3b5a86d86
Md Shamim Ahamed <md.shamim.ahamed.888@gmail.com>
2023-11-14 19:51:24 +0600

    init

---
+++ lib/ui/screens/splash_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SplashScreenState extends StatefulWidget {
+  const SplashScreenState({super.key});
+
+  @override
+  State<SplashScreenState> createState() => _SplashScreenStateState();
+}
+
+class _SplashScreenStateState extends State<SplashScreenState> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
